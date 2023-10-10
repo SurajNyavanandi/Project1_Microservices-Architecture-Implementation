eff973d5b3aa0b060581ac4362c6f94dd977f01c
SurajNyavanandi <147505010+SurajNyavanandi@users.noreply.github.com>
2023-10-10 17:50:53 +0530

    Add files via upload

---
+++ Project1-EurekaDiscoveryClient/src/main/java/com/eurekadiscoveryclient/Project1EurekaDiscoveryClientApplication.java
@@ -0,0 +1,13 @@
+package com.eurekadiscoveryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1EurekaDiscoveryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1EurekaDiscoveryClientApplication.class, args);
+	}
+
+}
---
+++ Project1-EurekaDiscoveryClient/src/test/java/com/eurekadiscoveryclient/Project1EurekaDiscoveryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurekadiscoveryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1EurekaDiscoveryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
