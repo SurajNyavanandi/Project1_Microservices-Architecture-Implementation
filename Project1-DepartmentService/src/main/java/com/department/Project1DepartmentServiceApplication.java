3567718939a2156b2768307cc497b37a61125683
SurajNyavanandi <147505010+SurajNyavanandi@users.noreply.github.com>
2023-10-10 17:53:31 +0530

    Add files via upload

---
+++ Project1-DepartmentService/src/main/java/com/department/Project1DepartmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.department;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1DepartmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1DepartmentServiceApplication.class, args);
+	}
+
+}
